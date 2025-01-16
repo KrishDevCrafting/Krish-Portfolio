580e4f34a704d3e4f04e274d70cd58805015ef06
KrishDevCrafting <krishkumer150@gmail.com>
2025-01-16 22:16:28 +0530

    {nav-bar}

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import App from "./App";
+import Main from "./home/Main";
 import Nav from "./NavBar";
 import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Nav />
+    <Main />
     <App />
   </React.StrictMode>
 );
