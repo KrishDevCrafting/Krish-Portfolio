5b6e47ce3a24a09c529fe3d2bd0cbc5c8c5e1c72
KrishDevCrafting <krishkumer150@gmail.com>
2025-01-18 23:06:21 +0530

    Half complete

--- my-app/src/home/Main.jsx
+++ my-app/src/home/Main.jsx
@@ -6,16 +6,27 @@ const Main = () => {
     <>
       <div className="container">
         <div className="container-child">
-        <h1>I'm Krish</h1>
+          <h1>I'm Krish</h1>
 
-        <h4>I'm Developer!</h4>
+          <h4>I'm Developer!</h4>
 
-        <button type="button">Get in touch--</button></div>
+          <button type="button">Get in touch--</button>
+          <button type="button">Resume</button>
+        </div>
         <div>
           <span>
             <img src={img} alt="" srcset="" id="img-size" />
           </span>
         </div>
+      </div>
+      <div className="experience-container">
+        <h1 style={{ textAlign: "center" }}>Experiences!</h1>
+        <ul>
+          <li>Javascript</li>
+          <li>React.js</li>
+          <li>Css</li>
+          <li>Express.js</li>
+        </ul>
       </div>
     </>
   );
