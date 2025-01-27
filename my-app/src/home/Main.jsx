d5ff6ccf8438f64e17ef337fce7d012d494e4550
Krish <krishkumer150@gmail.com>
2025-01-27 23:37:06 +0530

    Updated

--- my-app/src/home/Main.jsx
+++ my-app/src/home/Main.jsx
@@ -1,67 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import img from "../Assets/love.jpg";
 import "./index.css";
+
 const Main = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <>
-      <div className="container">
-        <div className="container-child">
-          <h1>I'm Krish</h1>
-
-          <h4>I'm Developer!</h4>
-
-          <button type="button">Get in touch--</button>
+    <div className={darkMode ? "container dark-mode" : "container light-mode"}>
+      <div className="container-child">
+        <h1 className="fade-in">I'm Krish</h1>
+        <h4 className="fade-in delay-1">I'm a Developer!</h4>
+        <p className="fade-in delay-2">
+          Passionate about building web applications and learning new
+          technologies.
+        </p>
+        <p className="fade-in delay-3">
+          I specialize in front-end development and have experience with various
+          frameworks and libraries.
+        </p>
+        <div className="buttons fade-in delay-4">
+          <button type="button">Get in touch</button>
           <button type="button">Resume</button>
         </div>
-        <div>
-          <span>
-            <img src={img} alt="" srcset="" id="img-size" />
-          </span>
-        </div>
+        <button
+          className="toggle-button fade-in delay-5"
+          onClick={toggleDarkMode}>
+          {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+        </button>
       </div>
-      <div className="experience-container">
-        <h1 style={{ textAlign: "center" }}>Experiences!</h1>
-        <ul>
-          <li>Javascript</li>
-          <li>React.js</li>
-          <li>Css</li>
-          <li>Express.js</li>
-        </ul>
+      <div className="image-container fade-in delay-6">
+        <img src={img} alt="Profile" id="img-size" />
       </div>
-      {/* Section End */}
-      <div className="container-box">
-        <div>
-          <h1 style={{ textAlign: "center" }}>Projects</h1>
-          <div className="mini-box"></div>
-          <div className="mini-box"></div>
-          <div className="mini-box"></div>
-          <div className="mini-box"></div>
-        </div>
-      </div>
-
-      <footer>
-        <div className="footer-container">
-          <h1>Contact</h1>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus,
-            itaque fugiat! Illum accusantium illo ducimus sequi corrupti,
-            voluptate non, sapiente voluptates, error et excepturi vitae totam
-            ut aliquid libero voluptas dolorum temporibus atque alias neque?
-            Aspernatur rem sit voluptatum ipsum harum voluptate expedita sunt
-            assumenda nulla dolores esse similique consequatur, corrupti
-            suscipit exercitationem, perferendis quo eius, aperiam totam
-            doloremque officiis iure cupiditate hic. Distinctio ea quas tempore
-            voluptas in velit odio ab reiciendis molestias et, minima
-            consequatur fuga voluptate praesentium necessitatibus! Repellat ad
-            fugiat, odit nam eum voluptatum dolores, quod soluta molestias
-            provident possimus. Eum ducimus accusantium delectus nobis! Aperiam?
-          </p>
-        </div>
-      </footer>
-    </>
+    </div>
   );
 };
 
 export default Main;
-
-// hello
