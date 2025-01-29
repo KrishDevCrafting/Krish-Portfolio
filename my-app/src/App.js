077f1d1c4ca796e5883eff0c01a24d4c876ce7d1
KrishDevCrafting <krishkumer150@gmail.com>
2025-01-29 22:10:29 +0530

    badminton

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,20 @@
+import React from "react";
+import { Calendar } from "bpk-component-calendar"; // Import the Calendar component
+import "./App.scss"; // Import the styles
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello-React-whatApp</h1>
+      <header className="App-header">
+        <h1>Flight Schedule</h1> {/* Header text */}
+      </header>
+      <div className="Calendar-container">
+        <Calendar /> {/* Display the calendar */}
+      </div>
+      <button className="Continue-button">Continue</button>{" "}
+      {/* Button below the calendar */}
     </div>
   );
 }
 
 export default App;
-
-// hasdglhasg
