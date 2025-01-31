d8c86fff68329d08630b8e84ba52f79b4ec4a4f2
KrishDevCrafting <krishkumer150@gmail.com>
2025-01-31 22:40:13 +0530

    Updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,5 @@
 import React from "react";
-import { Calendar } from "bpk-component-calendar"; // Import the Calendar component
-import "./App.scss"; // Import the styles
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Flight Schedule</h1> {/* Header text */}
-      </header>
-      <div className="Calendar-container">
-        <Calendar /> {/* Display the calendar */}
-      </div>
-      <button className="Continue-button">Continue</button>{" "}
-      {/* Button below the calendar */}
-    </div>
-  );
+export default function App() {
+  return <div>App</div>;
 }
-
-export default App;
