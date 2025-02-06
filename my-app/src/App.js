b966fbaa49047e4f9251bc23736fd3da3490d868
KrishDevCrafting <krishkumer150@gmail.com>
2025-02-06 20:00:14 +0530

    Get-Started

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,3 @@ import React from "react";
 export default function App() {
   return <div>App</div>;
 }
-// Kal pakka!
-// Really Sorry I'm Lost 🙂;
-// hy feel so low and not good enough to take or make any productive decisions or actions.
-//I have a Lot of stress and i Want to handle it myself and I know that know going to help me for this chaos where i am stuck in my life. but I take step make decision and actions start my life again with my own better form!
