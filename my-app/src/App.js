dd529d4534513b7a8a879a3ac053ca0f91934786
Krish <krishkumer150@gmail.com>
2025-02-13 14:22:27 +0530

    Updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { ThemeProvider } from "./home/ThemeContext";
 import Main from "./home/Main";
 import Nav from "./NavBar";
+import Contact from "./home/Form/contact";
 export default function App() {
   return (
     <ThemeProvider>
       <div>
         <Nav />
         <Main />
+        <Contact />
       </div>
     </ThemeProvider>
   );
---
+++ my-app/src/home/Form/contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="container">
+        <h1>Contact Us</h1>
+        <p>text-data!</p>
+        <button >Click</button>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
