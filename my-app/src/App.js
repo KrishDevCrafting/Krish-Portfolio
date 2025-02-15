41a368b6fecb83f6614f335ee65025f3f86682bc
KrishDevCrafting <krishkumer150@gmail.com>
2025-02-15 23:46:32 +0530

    updatd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import { ThemeProvider } from "./home/ThemeContext";
 import Main from "./home/Main";
 import Nav from "./NavBar";
-import Contact from "./home/Form/contact";
+import Myservice from "./home/Myservice";
+
 export default function App() {
   return (
     <ThemeProvider>
       <div>
         <Nav />
         <Main />
-        <Contact />
+        <Myservice/>
       </div>
     </ThemeProvider>
   );
---
+++ my-app/src/home/Myservice/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./index.css";
+const Myservice = () => {
+  return (
+    <>
+      <div className="container-service">
+        <div className="container-child-service">
+          <h1>My Service</h1>
+
+          <h2>Crafting Stories Through</h2>
+          <h2>design and innovation!</h2>
+        </div>
+      </div>
+      {/* section-2 */}
+      <div className="container-service-2">
+        <div className="container-child-2"></div>
+        <div className="container-child-2"></div>
+        <div className="container-child-2"> </div>
+      </div>
+    </>
+  );
+};
+
+export default Myservice;
