f4caa3653612f6ef031f8b167681c24083b08e65
KrishDevCrafting <krishkumer150@gmail.com>
2025-03-09 22:00:08 +0530

    Updated

--- my-app/src/home/AboutMe/index.jsx
+++ my-app/src/home/AboutMe/index.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function AboutMe() {
   return (
-    <div><h1>About-Me</h1></div>
-  )
+    <div>
+      <h1>About-Me</h1>
+
+      <h3>Transforming visions into exceptional portfolios</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda,
+        nostrum magni! Ipsum eligendi dicta exercitationem quae quo enim
+        deserunt quidem?
+      </p>
+
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
 }
