19a51d9a52a856b81f0e6228cb0828334a726d21
Krish <krishkumer150@gmail.com>
2025-03-12 19:50:42 +0530

    Updated

--- my-app/src/home/Skills/index.jsx
+++ my-app/src/home/Skills/index.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-
+import "./index.css";
 const PageSkill = () => {
   return (
     <>
       {/* section-Eduction and skills */}
       <div className="main-container">
-        <h3>EDUCATION & SKILL</h3>
+        <h3 className="heading-main">EDUCATION & SKILL</h3>
 
-        <h1>Showcasting your talent amplifying your impact! </h1>
+        <h1 className="heading-main">
+          Showcasting your talent amplifying your impact!
+        </h1>
 
-        <div>
-          <div>
+        <div className="main-container-child">
+          <div className="main-container-sub-child">
             <p className="box-design">2014-2018</p>
             <h5>WordPress and End Developer</h5>
             <p>
@@ -19,7 +21,7 @@ const PageSkill = () => {
               voluptatibus dicta error, quod sequi?
             </p>
           </div>
-          <div>
+          <div className="main-container-sub-child">
             <p className="box-design">2017-2019</p>
             <h5>Python Developer</h5>
             <p>
