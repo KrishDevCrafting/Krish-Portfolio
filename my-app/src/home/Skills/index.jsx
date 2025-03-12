0a9865d595b789b9059486ba983872fc64004761
Krish <krishkumer150@gmail.com>
2025-03-12 20:06:00 +0530

    Updated

--- my-app/src/home/Skills/index.jsx
+++ my-app/src/home/Skills/index.jsx
@@ -4,6 +4,7 @@ const PageSkill = () => {
   return (
     <>
       {/* section-Eduction and skills */}
+
       <div className="main-container">
         <h3 className="heading-main">EDUCATION & SKILL</h3>
 
@@ -11,6 +12,29 @@ const PageSkill = () => {
           Showcasting your talent amplifying your impact!
         </h1>
 
+        <div className="main-container-child">
+          <div className="main-container-sub-child">
+            <p className="box-design">2014-2018</p>
+            <h5>WordPress and End Developer</h5>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Reprehenderit voluptas adipisci natus dolor tenetur alias
+              voluptatibus dicta error, quod sequi?
+            </p>
+          </div>
+          <div className="main-container-sub-child">
+            <p className="box-design">2017-2019</p>
+            <h5>Python Developer</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est porro
+              veritatis quisquam tenetur, consectetur esse! Debitis iste
+              similique commodi praesentium?
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* second! */}
+      <div className="main-container">
         <div className="main-container-child">
           <div className="main-container-sub-child">
             <p className="box-design">2014-2018</p>
