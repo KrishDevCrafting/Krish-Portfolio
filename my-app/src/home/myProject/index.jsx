7635e28d3829dfc9ef62a190c4c33a16f8e57ec0
KrishDevCrafting <krishkumer150@gmail.com>
2025-04-03 23:03:02 +0530

    Updated

--- my-app/src/home/myProject/index.jsx
+++ my-app/src/home/myProject/index.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import "./index.css";
 const Myprojects = () => {
   return (
-    <div className="parent-container">
-      <h3>MY RECENT PORTFOLIO</h3>
-      <h1>
-        Elevate your brand to new
-        <br />
-        heights with our portfolio expertise
-      </h1>
+    <>
+      <div className="parent-container">
+        <h3>MY RECENT PORTFOLIO</h3>
+        <h1>
+          Elevate your brand to new
+          <br />
+          heights with our portfolio expertise
+        </h1>
+        <div className="project-parent">
+          <div className="project-card"></div>
+          <div className="project-card"> </div>
+        </div>
+      </div>
+      {/* second-line */}
       <div className="project-parent">
         <div className="project-card"></div>
         <div className="project-card"> </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Myprojects;
