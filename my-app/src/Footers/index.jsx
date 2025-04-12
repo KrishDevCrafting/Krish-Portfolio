13af608d015b00d6e3bfe17ed5a830acb229a237
KrishDevCrafting <krishkumer150@gmail.com>
2025-04-12 23:23:31 +0530

    Up

--- my-app/src/Footers/index.jsx
+++ my-app/src/Footers/index.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer
+      style={{
+        backgroundColor: "#282c34",
+        color: "white",
+        padding: "20px",
+        textAlign: "center",
+      }}
+    >
+      <div style={{ marginBottom: "20px" }} className="parent-footer">
+        <div>
+          <h1 style={{ fontSize: "18px", margin: "10px 0" }}>Address</h1>
+          <p>123 Main Street, City, Country</p>
+        </div>
+
+        <div style={{ marginBottom: "20px" }}>
+          <h1 style={{ fontSize: "18px", margin: "10px 0" }}>Let's Talk</h1>
+          <p>+123 456 7890</p>
+        </div>
+        <div>
+          <h1 style={{ fontSize: "18px", margin: "10px 0" }}>
+            Send Us an Email!
+          </h1>
+          <p>contact@example.com</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
