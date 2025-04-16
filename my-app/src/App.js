67e22da99dbc6480bcbd028db0c0f4eedea2040e
KrishDevCrafting <krishkumer150@gmail.com>
2025-04-16 22:35:22 +0530

    updated branch main

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,19 @@
 import React, { useRef } from "react";
 import { ThemeProvider } from "./home/ThemeContext";
 
-import Nav from "./NavBar";
+
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutSection from "./Testing/Test1";
 import Home from "./Home";
 export default function App() {
-  const aboutRef = useRef(null);
-
-  const scrollToAbout = () => {
-    aboutRef.current?.scrollIntoView({ behavior: "smooth" });
-  };
-
   return (
     <ThemeProvider>
       <Router>
-        <Nav onAboutclick={scrollToAbout} />
-
         {/* Routes block should ONLY contain <Route /> elements */}
         <Routes>
           <Route path="/" element={<Home />} />
-         
+
           <Route path="/test" element={<AboutSection />} />
         </Routes>
       </Router>
--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,16 +1,23 @@
-import react from "react";
+import React,{useRef} from "react";
 import Main from "./home/Main";
 import Myservice from "./home/Myservice";
 import AboutMe from "./home/AboutMe";
 import Myprojects from "./home/myProject";
 import Footer from "./Footers";
+import Nav from "./NavBar";
 const Home = () => {
+  const aboutRef = useRef(null);
+
+  const scrollToAbout = () => {
+    aboutRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <>
       <div>
+        <Nav onAboutclick={scrollToAbout} />
         <Main />
         <Myservice />
-        <AboutMe />
+        <AboutMe ref={aboutRef}/>
         <Myprojects />
         <Footer />
       </div>
