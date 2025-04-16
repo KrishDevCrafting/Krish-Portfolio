c79a9f11baf49a17f6c8b2fd772bdbf2f20eef3c
KrishDevCrafting <krishkumer150@gmail.com>
2025-04-16 23:33:38 +0530

    updated

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,4 +1,4 @@
-import React,{useRef} from "react";
+import React, { useRef } from "react";
 import Main from "./home/Main";
 import Myservice from "./home/Myservice";
 import AboutMe from "./home/AboutMe";
@@ -7,18 +7,20 @@ import Footer from "./Footers";
 import Nav from "./NavBar";
 const Home = () => {
   const aboutRef = useRef(null);
-
+  const projectRef = useRef(null);
   const scrollToAbout = () => {
     aboutRef.current?.scrollIntoView({ behavior: "smooth" });
+    projectRef.current?.scrollIntoView({ behavior: "smooth" });
   };
+
   return (
     <>
       <div>
         <Nav onAboutclick={scrollToAbout} />
         <Main />
         <Myservice />
-        <AboutMe ref={aboutRef}/>
-        <Myprojects />
+        <AboutMe ref={aboutRef} />
+        <Myprojects ref={projectRef} />
         <Footer />
       </div>
     </>
