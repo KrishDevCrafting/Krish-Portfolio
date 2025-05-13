94ce7e5d446fbccc1740569f89f044cc4a69a936
KrishDevCrafting <krishkumer150@gmail.com>
2025-05-13 23:27:11 +0530

    changes on form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,6 @@
 import React, { useRef } from "react";
 import { ThemeProvider } from "./home/ThemeContext";
-
-
+import Form from "./contactFrom/index";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutSection from "./Testing/Test1";
@@ -13,7 +12,7 @@ export default function App() {
         {/* Routes block should ONLY contain <Route /> elements */}
         <Routes>
           <Route path="/" element={<Home />} />
-
+          <Route path="/contact" element={<Form />} />
           <Route path="/test" element={<AboutSection />} />
         </Routes>
       </Router>
