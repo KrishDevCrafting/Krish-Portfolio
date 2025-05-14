e508d0e86613fb97dd242c8a542627f6f3a5e39c
KrishDevCrafting <krishkumer150@gmail.com>
2025-05-14 23:58:28 +0530

    bug-issue

--- my-app/src/contactFrom/index.jsx
+++ my-app/src/contactFrom/index.jsx
@@ -1,31 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import styled from "styled-components";
 import "./index.css";
 const Form = () => {
-  return (
-    <div className="login-box">
-      <form>
-        <div className="user-box">
-          <input type="text" name required />
-          <label>Full Name</label>
-        </div>
-        <div className="user-box">
-          <input type="password" name required />
-          <label>Email</label>
-        </div>
+  const [theme, setTheme] = useState(false);
 
-        <div className="user-box">
-          <input type="text" name required />
-          <label>Message</label>
-        </div>
-        <center>
-          <a href="#">
-            Connected
-            <span />
-          </a>
-        </center>
-      </form>
-    </div>
+  const handleMode = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+  useEffect(() => {
+    document.body.className = ""; // Clear previous classes
+    document.body.classList.add(theme);
+  }, [theme]);
+
+  return (
+    <>
+      <div>
+        <button type="submit" onClick={handleMode}>
+          {theme === "light" ? "Dark" : "Light"}
+          Switch
+        </button>
+      </div>
+
+      <div className="login-box">
+        <form>
+          <div className="user-box">
+            <input type="text" name required />
+            <label>Full Name</label>
+          </div>
+          <div className="user-box">
+            <input type="password" name required />
+            <label>Email</label>
+          </div>
+
+          <div className="user-box">
+            <input type="text" name required />
+            <label>Message</label>
+          </div>
+          <center>
+            <a href="#">
+              Connected
+              <span />
+            </a>
+          </center>
+        </form>
+      </div>
+    </>
   );
 };
 
