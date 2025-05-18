06fa76c3be496b56196e01123f1becfa04fc0fe9
KrishDevCrafting <krishkumer150@gmail.com>
2025-05-18 22:43:51 +0530

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,21 +4,20 @@ import Form from "./contactFrom/index";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutSection from "./Testing/Test1";
-// import Home from "./Home";
-// export default function App() {
-//   return (
-//     <ThemeProvider>
-//       <Router>
-//         {/* Routes block should ONLY contain <Route /> elements */}
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/contact" element={<Form />} />
-//           <Route path="/test" element={<AboutSection />} />
-//         </Routes>
-//       </Router>
-//     </ThemeProvider>
-//   );
-// }
+import Home from "./Home";
+export default function App() {
+  return (
+    <ThemeProvider>
+      <Router>
+        {/* Routes block should ONLY contain <Route /> elements */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Form />} />
+          <Route path="/test" element={<AboutSection />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
+}
 
 
-// 
